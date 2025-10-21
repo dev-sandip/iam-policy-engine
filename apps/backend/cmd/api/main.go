0bd3f67950c13c2711c57cacfb6d48319947cb5f
Sandip Sapkota <sandipsapkota9844@gmail.com>
2025-10-21 11:41:27 +0545

    basic setup

---
+++ apps/backend/cmd/api/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		 log.Print("hello world")
+		return c.String(http.StatusOK, "Welcome to IAM Policy Engine API")
+	})
+	
+	e.Logger.Fatal(e.Start(":1323"))
+}
